cee4aea3fbf966e54998367bbbe75ab599837307
unknown <buinhupha@gmail.com>
2024-04-12 16:33:41 +0700

    initial pj

---
+++ src/components/layout/header/header.layout.tsx
@@ -0,0 +1,6 @@
+
+export default function headerLayout() {
+  return (
+    <header>header.layout</header>
+  )
+}
---
+++ src/components/layout/navbar/navbar.layout.tsx
@@ -0,0 +1,7 @@
+
+
+export default function navbarLayout() {
+  return (
+    <div>navbar</div>
+  )
+}
